b18635f7a35619a539957b614480f589915e8ba8
gonmpr <gmporrorosas@gmail.com>
2026-05-28 22:33:33 +0000

    opcode decoding

--- dasm.c
+++ dasm.c
@@ -1,1 +1,89 @@
+#include "dasm.h"
+
+const opcode_t op_set[] = {
+
+  {.mask = 0b11111100, .pattern = 0b10001000, .kind = OP_MOV}, // REG -> REG/MEM
+  {.mask = 0b11111110, .pattern = 0b11000110, .kind = OP_MOV}, // INM -> REG/MEM
+  {.mask = 0b11110000, .pattern = 0b10110000, .kind = OP_MOV}, // INM -> REG
+
+  {.mask = 0b11111100, .pattern = 0b00000000, .kind = OP_ADD}, // REG/MEM + REG
+  {.mask = 0b11111110, .pattern = 0b00000100, .kind = OP_ADD}, // ACC + INM
+
+  {.mask = 0b11111100, .pattern = 0b00101000, .kind = OP_SUB}, // REG/MEM + REG
+  {.mask = 0b11111110, .pattern = 0b00101100, .kind = OP_SUB}, // ACC + INM
+
+  {.mask = 0b11111100, .pattern = 0b00111000, .kind = OP_CMP}, // REG/MEM + REG
+  {.mask = 0b11111110, .pattern = 0b00111100, .kind = OP_CMP}, // ACC + INM
+
+  // REG/MEM + INM, all three share the same pattern, they diff each other with
+  // the reg field, 111 = cmp, 101 = sub, 000 = add
+  {.mask = 0b11111100, .pattern = 0b10000000, .kind = OP_INM_RM}, 
+
+  {.mask = 0b11111111, .pattern = 0b01110101, .kind = OP_JNZ},
+};
+
+
+const size_t op_count = sizeof(op_set) / sizeof(op_set[0]);
+
+
+//first decode opcode
+opcode_t get_opcode(unsigned char byte){
+
+  for(size_t i=0; i<insc; i++){
+    opcode_t op = op_set[i];
+
+    if ((byte & op.mask) == op.pattern){
+      return op;
+    }
+
+  }
+
+  opcode_t invalid = {0};
+  return invalid;
+}
+
+//then decode things in base of opcode
+instruction_t decode_instruction(memory_t *mem){
+
+    instruction_t ins = {0};
+    ins.opcode = get_opcode(mem->data[mem->offset]);
+
+    switch (ins.opcode.kind) {
+
+        case OP_INVALID:
+        {
+          return ins;
+        }
+
+        case OP_MOV:
+        {
+            break;
+        }
+
+        case OP_ADD:
+        {
+            break;
+        }
+
+        case OP_SUB:
+        {
+            break;
+        }
+
+        case OP_CMP:
+        {
+            break;
+        }
+
+        case OP_JNZ:
+        {
+            break;
+        }
+    }
+
+    return ins;
+}
+
+
+
 
--- dasm.h
+++ dasm.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+//just renaming int types
 typedef uint8_t   u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -13,11 +14,12 @@ typedef int32_t  s32;
 typedef int64_t  s64;
 
 
+//registers
 typedef enum {
     REG_A,
+    REG_B,
     REG_C,
     REG_D,
-    REG_B,
     REG_SP,
     REG_BP,
     REG_SI,
@@ -25,20 +27,50 @@ typedef enum {
 } reg_t;
 
 
+// modes for the instruction
 typedef enum {
   MEM0_MOD, MEM8_MOD, MEM16_MOD, REG_MOD 
 } mod_t;
 
-
+// for the instruction operands
 typedef enum{
   OPERAND_ADDRESS,
   OPERAND_IMMEDIATE,
   OPERAND_REGISTER,
 }operand_kind_t;
 
+// for instruction decoding in opcode_t struct
+typedef enum{
+  OP_INVALID,
+  OP_INM_RM,
+  OP_MOV,
+  OP_ADD,
+  OP_SUB,
+  OP_CMP,
+  OP_JNZ,
+}instruction_kind_t;
 
+
+
+
+
+
+
+//for the effective address calculation
+typedef struct {
+
+    reg_t base;
+    reg_t index;
+
+    u16 displacement;
+
+} memory_operand_t;
+
+
+// for the operands of the instruction
 typedef struct {
   operand_kind_t kind;
+  bool w;
 
   union {
       u16 immediate;
@@ -48,19 +80,31 @@ typedef struct {
 } operand_t;
 
 
-
-
+// the opcode for decoding
 typedef struct {
 
-  u8 opcode;
-  mod_t mod;
+    u8 mask;
+    u8 pattern;
+
+    instruction_kind_t kind;
+
+} opcode_t;
+
+
+//the instruction representation, used for printing the text
+typedef struct {
+
+  opcode_t opcode;
   u8 size;
 
-  operand_t operands[2];
+  mod_t mod;
+  operand_t dst;
+  operand_t src;
 
 } instruction_t;
 
 
 
+opcode_t get_opcode(unsigned char byte);
 
-
+instruction_t decode_instruction(memory_t *mem);
